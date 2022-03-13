b9c3ffaaac9f25305af54a4ad1135338f3853e5b
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-03-13 16:18:31 +0100

    XML summary documentation and ArgumentNullException.

--- ListShuffle/ListExtensions.cs
+++ ListShuffle/ListExtensions.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ListShuffle
 {
+    /// <summary>
+    /// ListShuffle extension class
+    /// </summary>
     public static class ListExtensions
     {
+        /// <summary>
+        /// Thread-safe shuffle of all items in the list.
+        /// </summary>
+        /// <typeparam name="T">The generic type of the list to extend.</typeparam>
+        /// <param name="list">The list to extend.</param>
         public static void Shuffle<T>(this IList<T> list)
         {
+            if (list == null) throw new ArgumentNullException(nameof(list));
             list = list.OrderBy(i => ThreadSafeRandom.Next()).ToList();
         }
     }
