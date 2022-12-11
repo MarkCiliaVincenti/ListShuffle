dbe81b12200852bbae00ec0635edfd51831e6427
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-12-11 10:13:13 +0100

    Micro performance improvements.

--- ListShuffle/ListExtensions.cs
+++ ListShuffle/ListExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using ThreadSafeRandomizer;
 
@@ -16,14 +17,14 @@ namespace ListShuffle
         /// <typeparam name="T">The generic type of the list to extend.</typeparam>
         /// <param name="list">The list to extend.</param>
         /// <exception cref="ArgumentNullException"><paramref name="list"/> is null.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Shuffle<T>(this IList<T> list)
         {
             if (list == null) throw new ArgumentNullException(nameof(list));
             int n = list.Count;
             while (n > 1)
             {
-                n--;
-                int k = ThreadSafeRandom.Instance.Next(n + 1);
+                int k = ThreadSafeRandom.Instance.Next(n--);
                 (list[n], list[k]) = (list[k], list[n]);
             }
         }
@@ -34,6 +35,7 @@ namespace ListShuffle
         /// <typeparam name="T">The generic type of the list to extend.</typeparam>
         /// <param name="list">The list to extend.</param>
         /// <exception cref="ArgumentNullException"><paramref name="list"/> is null.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CryptoStrongShuffle<T>(this IList<T> list)
         {
             if (list == null) throw new ArgumentNullException(nameof(list));
@@ -43,15 +45,13 @@ namespace ListShuffle
 #endif
             while (n > 1)
             {
-                n--;
-
 #if NETSTANDARD2_1
-                int k = RandomNumberGenerator.GetInt32(n + 1);
+                int k = RandomNumberGenerator.GetInt32(n--);
 #else
                 var data = new byte[sizeof(uint)];
                 generator.GetBytes(data);
                 var randUint = BitConverter.ToUInt32(data, 0);
-                int k = (int)Math.Floor((((double)n + 1) * (randUint / (uint.MaxValue + 1.0))));
+                int k = (int)Math.Floor(n-- * (randUint / (uint.MaxValue + 1.0)));
 #endif
                 (list[n], list[k]) = (list[k], list[n]);
             }
