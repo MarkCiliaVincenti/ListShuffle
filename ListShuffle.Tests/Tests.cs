45b1a3c38a7440a12a665d8d20f3c095bb9a6f8e
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2025-05-31 19:17:31 +0200

    Dependency update.

--- ListShuffle.Tests/Tests.cs
+++ ListShuffle.Tests/Tests.cs
@@ -5,266 +5,265 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace ListShuffle.Tests.Net50
+namespace ListShuffle.Tests;
+
+public class Tests
 {
-    public class Tests
+    [Fact]
+    public void TestOneList()
     {
-        [Fact]
-        public void TestOneList()
+        var list = new List<int>();
+        for (int j = 1; j <= 10000; j++)
+        {
+            list.Add(j);
+        }
+
+        list.Shuffle();
+
+        if (list.First() == 1)
+        {
+            list.Shuffle();
+        }
+
+        Assert.False(list.First() == 1);
+    }
+
+    [Fact]
+    public void TestOneArray()
+    {
+        var array = new int[10000];
+        for (int j = 0; j < 10000;)
+        {
+            array[j] = ++j;
+        }
+
+        array.Shuffle();
+
+        if (array.First() == 1)
+        {
+            array.Shuffle();
+        }
+
+        Assert.False(array.First() == 1);
+    }
+
+    [Fact]
+    public void TestOneSpan()
+    {
+        Span<int> span = stackalloc int[10000];
+        for (int j = 0; j < 10000;)
+        {
+            span[j] = ++j;
+        }
+
+        span.Shuffle();
+
+        if (span[0] == 1)
+        {
+            span.Shuffle();
+        }
+
+        Assert.False(span[0] == 1);
+    }
+
+    [Fact]
+    public void TestConcurrencyList()
+    {
+        int totalFives = 0;
+
+        Parallel.For(1, 100001, (_, _) =>
         {
             var list = new List<int>();
-            for (int j = 1; j <= 10000; j++)
+            for (int j = 1; j <= 10; j++)
             {
                 list.Add(j);
             }
 
             list.Shuffle();
 
-            if (list.First() == 1)
+            if (list.First() == 5)
             {
-                list.Shuffle();
+                Interlocked.Increment(ref totalFives);
             }
+        });
 
-            Assert.False(list.First() == 1);
-        }
+        Assert.InRange(totalFives, 5000, 20000);
+    }
 
-        [Fact]
-        public void TestOneArray()
+    [Fact]
+    public void TestConcurrencyArray()
+    {
+        int totalFives = 0;
+
+        Parallel.For(1, 100001, (_, _) =>
         {
-            var array = new int[10000];
-            for (int j = 0; j < 10000;)
+            var array = new int[10];
+            for (int j = 0; j < 10;)
             {
                 array[j] = ++j;
             }
 
             array.Shuffle();
 
-            if (array.First() == 1)
+            if (array.First() == 5)
             {
-                array.Shuffle();
+                Interlocked.Increment(ref totalFives);
             }
+        });
 
-            Assert.False(array.First() == 1);
-        }
+        Assert.InRange(totalFives, 5000, 20000);
+    }
 
-        [Fact]
-        public void TestOneSpan()
+    [Fact]
+    public void TestConcurrencySpan()
+    {
+        int totalFives = 0;
+
+        Parallel.For(1, 100001, (_, _) =>
         {
-            Span<int> span = stackalloc int[10000];
-            for (int j = 0; j < 10000;)
+            Span<int> span = stackalloc int[10];
+            for (int j = 0; j < 10;)
             {
                 span[j] = ++j;
             }
 
             span.Shuffle();
 
-            if (span[0] == 1)
+            if (span[0] == 5)
             {
-                span.Shuffle();
+                Interlocked.Increment(ref totalFives);
             }
+        });
 
-            Assert.False(span[0] == 1);
-        }
+        Assert.InRange(totalFives, 5000, 20000);
+    }
 
-        [Fact]
-        public void TestConcurrencyList()
+    [Fact]
+    public void TestOneCryptoStrongList()
+    {
+        var list = new List<int>();
+        for (int j = 1; j <= 10000; j++)
         {
-            int totalFives = 0;
-
-            Parallel.For(1, 100001, (_, _) =>
-            {
-                var list = new List<int>();
-                for (int j = 1; j <= 10; j++)
-                {
-                    list.Add(j);
-                }
-
-                list.Shuffle();
-
-                if (list.First() == 5)
-                {
-                    Interlocked.Increment(ref totalFives);
-                }
-            });
-
-            Assert.InRange(totalFives, 5000, 20000);
+            list.Add(j);
         }
 
-        [Fact]
-        public void TestConcurrencyArray()
+        list.CryptoStrongShuffle();
+
+        if (list.First() == 1)
         {
-            int totalFives = 0;
-
-            Parallel.For(1, 100001, (_, _) =>
-            {
-                var array = new int[10];
-                for (int j = 0; j < 10;)
-                {
-                    array[j] = ++j;
-                }
-
-                array.Shuffle();
-
-                if (array.First() == 5)
-                {
-                    Interlocked.Increment(ref totalFives);
-                }
-            });
-
-            Assert.InRange(totalFives, 5000, 20000);
+            list.Shuffle();
         }
 
-        [Fact]
-        public void TestConcurrencySpan()
+        Assert.False(list.First() == 1);
+    }
+
+    [Fact]
+    public void TestOneCryptoStrongArray()
+    {
+        var array = new int[10000];
+        for (int j = 0; j < 10000;)
         {
-            int totalFives = 0;
-
-            Parallel.For(1, 100001, (_, _) =>
-            {
-                Span<int> span = stackalloc int[10];
-                for (int j = 0; j < 10;)
-                {
-                    span[j] = ++j;
-                }
-
-                span.Shuffle();
-
-                if (span[0] == 5)
-                {
-                    Interlocked.Increment(ref totalFives);
-                }
-            });
-
-            Assert.InRange(totalFives, 5000, 20000);
+            array[j] = ++j;
         }
 
-        [Fact]
-        public void TestOneCryptoStrongList()
+        array.CryptoStrongShuffle();
+
+        if (array.First() == 1)
+        {
+            array.CryptoStrongShuffle();
+        }
+
+        Assert.False(array.First() == 1);
+    }
+
+    [Fact]
+    public void TestOneCryptoStrongSpan()
+    {
+        Span<int> span = stackalloc int[10000];
+        for (int j = 0; j < 10000;)
+        {
+            span[j] = ++j;
+        }
+
+        span.CryptoStrongShuffle();
+
+        if (span[0] == 1)
+        {
+            span.CryptoStrongShuffle();
+        }
+
+        Assert.False(span[0] == 1);
+    }
+
+    [Fact]
+    public void TestConcurrencyCryptoStrongList()
+    {
+        int totalFives = 0;
+
+        Parallel.For(1, 100001, (_, _) =>
         {
             var list = new List<int>();
-            for (int j = 1; j <= 10000; j++)
+            for (int j = 1; j <= 10; j++)
             {
                 list.Add(j);
             }
 
             list.CryptoStrongShuffle();
 
-            if (list.First() == 1)
+            if (list.First() == 5)
             {
-                list.Shuffle();
+                Interlocked.Increment(ref totalFives);
             }
+        });
 
-            Assert.False(list.First() == 1);
-        }
+        Assert.InRange(totalFives, 5000, 20000);
+    }
 
-        [Fact]
-        public void TestOneCryptoStrongArray()
+    [Fact]
+    public void TestConcurrencyCryptoStrongArray()
+    {
+        int totalFives = 0;
+
+        Parallel.For(1, 100001, (_, _) =>
         {
-            var array = new int[10000];
-            for (int j = 0; j < 10000;)
+            var array = new int[10];
+            for (int j = 0; j < 10;)
             {
                 array[j] = ++j;
             }
 
             array.CryptoStrongShuffle();
 
-            if (array.First() == 1)
+            if (array.First() == 5)
             {
-                array.CryptoStrongShuffle();
+                Interlocked.Increment(ref totalFives);
             }
+        });
 
-            Assert.False(array.First() == 1);
-        }
+        Assert.InRange(totalFives, 5000, 20000);
+    }
 
-        [Fact]
-        public void TestOneCryptoStrongSpan()
+    [Fact]
+    public void TestConcurrencyCryptoStrongSpan()
+    {
+        int totalFives = 0;
+
+        Parallel.For(1, 100001, (_, _) =>
         {
-            Span<int> span = stackalloc int[10000];
-            for (int j = 0; j < 10000;)
+            Span<int> span = stackalloc int[10];
+            for (int j = 0; j < 10;)
             {
                 span[j] = ++j;
             }
 
             span.CryptoStrongShuffle();
 
-            if (span[0] == 1)
+            if (span[0] == 5)
             {
-                span.CryptoStrongShuffle();
+                Interlocked.Increment(ref totalFives);
             }
+        });
 
-            Assert.False(span[0] == 1);
-        }
-
-        [Fact]
-        public void TestConcurrencyCryptoStrongList()
-        {
-            int totalFives = 0;
-
-            Parallel.For(1, 100001, (_, _) =>
-            {
-                var list = new List<int>();
-                for (int j = 1; j <= 10; j++)
-                {
-                    list.Add(j);
-                }
-
-                list.CryptoStrongShuffle();
-
-                if (list.First() == 5)
-                {
-                    Interlocked.Increment(ref totalFives);
-                }
-            });
-
-            Assert.InRange(totalFives, 5000, 20000);
-        }
-
-        [Fact]
-        public void TestConcurrencyCryptoStrongArray()
-        {
-            int totalFives = 0;
-
-            Parallel.For(1, 100001, (_, _) =>
-            {
-                var array = new int[10];
-                for (int j = 0; j < 10;)
-                {
-                    array[j] = ++j;
-                }
-
-                array.CryptoStrongShuffle();
-
-                if (array.First() == 5)
-                {
-                    Interlocked.Increment(ref totalFives);
-                }
-            });
-
-            Assert.InRange(totalFives, 5000, 20000);
-        }
-
-        [Fact]
-        public void TestConcurrencyCryptoStrongSpan()
-        {
-            int totalFives = 0;
-
-            Parallel.For(1, 100001, (_, _) =>
-            {
-                Span<int> span = stackalloc int[10];
-                for (int j = 0; j < 10;)
-                {
-                    span[j] = ++j;
-                }
-
-                span.CryptoStrongShuffle();
-
-                if (span[0] == 5)
-                {
-                    Interlocked.Increment(ref totalFives);
-                }
-            });
-
-            Assert.InRange(totalFives, 5000, 20000);
-        }
+        Assert.InRange(totalFives, 5000, 20000);
     }
 }
